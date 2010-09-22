5af79c418ae03545a62ca379127e23cdc81088dc
YuLei Liao <dualface@gmail.com>
2010-09-23 00:02:09 +0800

    增加 tests/ModelEventsTest

---
+++ tests/cases/storage/ModelEventsTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace tests\cases\storage;
+
+use tests\includes\TestCase;
+
+require_once __DIR__ . '/__init.php';
+
+class ModelEventsTest extends ModelTestHelper
+{
+    //    /* 事件 */
+    //    /* class events */
+    //    const BEFORE_FINDONE_EVENT  = '__before_findone';
+    //    const AFTER_FINDONE_EVENT   = '__after_findone';
+    //    const BEFORE_FINDMULTI_EVENT = '__before_findmulti';
+    //    const AFTER_FINDMULTI_EVENT = '__after_findmulti';
+    //    const BEFORE_ERASE_EVENT    = '__before_erase';
+    //    const AFTER_ERASE_EVENT     = '__after_erase';
+    //    /* instance events */
+    //    const BEFORE_SAVE_EVENT     = '__before_save';
+    //    const AFTER_SAVE_EVENT      = '__after_save';
+    //    const BEFORE_CREATE_EVENT   = '__before_create';
+    //    const AFTER_CREATE_EVENT    = '__after_create';
+    //    const BEFORE_UPDATE_EVENT   = '__before_update';
+    //    const AFTER_UPDATE_EVENT    = '__after_update';
+    //    const BEFORE_DEL_EVENT      = '__before_del';
+    //    const AFTER_DEL_EVENT       = '__after_del';
+
+    function test_findone_event()
+    {
+        $this->markTestIncomplete(__METHOD__);
+    }
+
+    function test_findmulti_event()
+    {
+        $this->markTestIncomplete(__METHOD__);
+    }
+
+    function test_erase_event()
+    {
+        $this->markTestIncomplete(__METHOD__);
+    }
+
+    function test_create_event()
+    {
+        $this->markTestIncomplete(__METHOD__);
+    }
+
+    function test_update_event()
+    {
+        $this->markTestIncomplete(__METHOD__);
+    }
+
+    function test_del_event()
+    {
+        $this->markTestIncomplete(__METHOD__);
+    }
+}
