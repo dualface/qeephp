60724a7f11994aa827abb2c9d726a61ab3ec54b9
YuLei Liao <dualface@gmail.com>
2010-09-24 03:13:50 +0800

    BaseError 异常增加 invalid_parameters_error() 和 unknown_error()

--- packages/qeephp/BaseError.php
+++ packages/qeephp/BaseError.php
@@ -7,54 +7,45 @@ namespace qeephp;
  */
 class BaseError extends \Exception
 {
-    const NOT_IMPLEMENTED = 0x9991;
-    const TYPE_MISMATCH   = 0x9992;
-    const NOT_CALLABLE    = 0x9993;
+    const NOT_IMPLEMENTED       = 0x9991;
+    const TYPE_MISMATCH         = 0x9992;
+    const NOT_CALLABLE          = 0x9993;
+    const INVALID_PARAMETERS    = 0x9994;
+    const UNKNOWN_ERROR         = 0x9995;
 
-    /**
-     * 构造函数
-     *
-     * @param string $message
-     * @param int $code
-     */
     function __construct($message, $code = 0)
     {
         parent::__construct($message, $code);
     }
 
-    /**
-     * 尝试调用未实现的方法
-     *
-     * @param string $method_name
-     *
-     * @return BaseError
-     */
     static function not_implemented_error($method_name)
     {
-        return new BaseError("NOT_IMPLEMENTED: {$method_name}", self::NOT_IMPLEMENTED);
+        $class = get_called_class();
+        return new $class("NOT_IMPLEMENTED: {$method_name}", self::NOT_IMPLEMENTED);
     }
 
-    /**
-     * 类型不匹配
-     *
-     * @param string $expected_type
-     * @param string $actual_type
-     * 
-     * @return BaseError
-     */
     static function type_mismatch_error($expected_type, $actual_type)
     {
-        return new BaseError("TYPE_MISMATCH: {$expected_type}, {$actual_type}", self::TYPE_MISMATCH);
+        $class = get_called_class();
+        return new $class("TYPE_MISMATCH: {$expected_type}, {$actual_type}", self::TYPE_MISMATCH);
     }
 
-    /**
-     * 无法调用的对象
-     *
-     * @return BaseError
-     */
     static function not_callable_error()
     {
-        return new BaseError("NOT_CALLABLE", self::NOT_CALLABLE);
+        $class = get_called_class();
+        return new $class("NOT_CALLABLE", self::NOT_CALLABLE);
+    }
+
+    static function invalid_parameters_error($message)
+    {
+        $class = get_called_class();
+        return new $class($message, self::INVALID_PARAMETERS);
+    }
+
+    static function unknown_error($message)
+    {
+        $class = get_called_class();
+        return new $class($message, self::UNKNOWN_ERROR);
     }
 }
 
