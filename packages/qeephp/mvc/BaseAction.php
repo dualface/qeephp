523bcf073705551f2f696c9068d94ff1db0c5aba
YuLei Liao <dualface@gmail.com>
2010-11-02 02:36:16 +0800

    为 mvc/BaseAction 增加对输入输出数据的验证支持

--- packages/qeephp/mvc/BaseAction.php
+++ packages/qeephp/mvc/BaseAction.php
@@ -22,6 +22,13 @@ abstract class BaseAction
     public $name;
 
     /**
+     * 当前请求
+     *
+     * @var Request
+     */
+    public $request;
+
+    /**
      * 执行结果
      *
      * @var mixed
@@ -34,10 +41,11 @@ abstract class BaseAction
      * @param App $app
      * @param string $name
      */
-    function __construct(App $app, $name)
+    function __construct($app, $name)
     {
         $this->app = $app;
         $this->name = $name;
+        $this->request = $app->request;
     }
 
     /**
@@ -46,8 +54,19 @@ abstract class BaseAction
     function __execute()
     {
         if (!$this->__before_execute()) return;
-        $result = $this->execute();
-        if (!is_null($result)) $this->result = $result;
+        if ($this->validate_input())
+        {
+            $result = $this->execute();
+            if (!is_null($result)) $this->result = $result;
+            if (!$this->validate_output())
+            {
+                $this->on_validate_output_failed();
+            }
+        }
+        else
+        {
+            $this->on_validate_input_failed();
+        }
         $this->__after_execute();
     }
 
@@ -70,6 +89,44 @@ abstract class BaseAction
      * @return mixed
      */
     abstract function execute();
+
+    /**
+     * 继承类覆盖此方法，用于在执行请求前过滤并验证输入数据
+     *
+     * 如果返回 false 则阻止调用 execute() 方法，并调用 validate_input_failed() 方法。
+     *
+     * @return bool
+     */
+    function validate_input()
+    {
+        return true;
+    }
+
+    /**
+     * 继承类覆盖此方法，用于在执行请求后过滤并验证输出数据
+     *
+     * 如果返回 false 则调用 validate_output_failed() 方法。
+     *
+     * @return bool
+     */
+    function validate_output()
+    {
+        return true;
+    }
+
+    /**
+     * 请求前对数据进行验证失败时调用此方法
+     */
+    function on_validate_input_failed()
+    {
+    }
+
+    /**
+     * 请求执行后对数据进行验证失败时调用此方法
+     */
+    function on_validate_output_failed()
+    {
+    }
 
     /**
      * 执行动作之前调用，如果返回 false 则阻止动作的执行
