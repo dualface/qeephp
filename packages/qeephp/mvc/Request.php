f4f8b681f4236005735c96722dddd73b936a8b68
YuLei Liao <dualface@gmail.com>
2010-11-02 03:05:47 +0800

    为 mvc/Request 添加注释

--- packages/qeephp/mvc/Request.php
+++ packages/qeephp/mvc/Request.php
@@ -2,49 +2,97 @@
 
 namespace qeephp\mvc;
 
+/**
+ * 封装一个请求
+ */
 class Request
 {
+    /**
+     * GET 数据
+     *
+     * @var array
+     */
     public $get;
+
+    /**
+     * POST 数据
+     *
+     * @var array
+     */
     public $post;
+
+    /**
+     * COOKIE 数据
+     *
+     * @var array
+     */
     public $cookie;
+
+    /**
+     * SESSION 数据
+     *
+     * @var array
+     */
     public $session;
 
     function __construct($get, $post, $cookie, $session)
     {
-        $this->get = $get;
-        $this->post = $post;
-        $this->cookie = $cookie;
+        $this->get     = $get;
+        $this->post    = $post;
+        $this->cookie  = $cookie;
         $this->session = $session;
     }
 
+    /**
+     * 从 GET 取得数据，如果指定数据不存在则返回 $default 指定的默认值
+     *
+     * @param string $name
+     * @param mixed $default
+     *
+     * @return mixed
+     */
     function get($name, $default = null)
     {
         return isset($this->get[$name]) ? $this->get[$name] : $default;
     }
 
+    /**
+     * 从 POST 取得数据，如果指定数据不存在则返回 $default 指定的默认值
+     *
+     * @param string $name
+     * @param mixed $default
+     *
+     * @return mixed
+     */
     function post($name, $default = null)
     {
         return isset($this->post[$name]) ? $this->post[$name] : $default;
     }
 
+    /**
+     * 从 COOKIE 取得数据，如果指定数据不存在则返回 $default 指定的默认值
+     *
+     * @param string $name
+     * @param mixed $default
+     *
+     * @return mixed
+     */
     function cookie($name, $default = null)
     {
         return isset($this->cookie[$name]) ? $this->cookie[$name] : $default;
     }
 
+    /**
+     * 从 SESSION 取得数据，如果指定数据不存在则返回 $default 指定的默认值
+     *
+     * @param string $name
+     * @param mixed $default
+     *
+     * @return mixed
+     */
     function session($name, $default = null)
     {
         return isset($this->session[$name]) ? $this->session[$name] : $default;
-    }
-
-    function filter($source, $rule)
-    {
-
-    }
-
-    function filter_all($batch)
-    {
-
     }
 }
 
